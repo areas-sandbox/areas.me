0f5561d8db906494cf13c5fb3ceeeb573ec84b93
Christian Areas <me@areas.me>
2022-12-13 02:16:46 -0800

    Installed gatsby-theme-portfolio-minimal.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,13 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `areas.me`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://areas.me`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
+  // If you use VS Code you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [`gatsby-theme-portfolio-minimal`],
 }
 
 export default config
